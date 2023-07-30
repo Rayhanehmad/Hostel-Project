667ce337d18d2cbec19f54c71e86c21c31188137
Rayhanehmand <reanahmed@yahoo.com>
2023-07-30 12:19:05 +0000

    added new project

---
+++ admin/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="admin";
+$dbpass="rehan123";
+$host="php-database.cuh4xxgciodi.us-east-1.rds.amazonaws.com";
+$db="Hostel";
+$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="rehan";
+$dbpass="admin123";
+$host="localhost";
+$db="student";
+$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
